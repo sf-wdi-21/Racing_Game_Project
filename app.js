ee26f83e03d4a27cc7d1d2d760b58f8e231a9a64
Nathan Allen <thenathanator@gmail.com>
2015-05-07 16:31:06 -0700

    clarification of js

--- app.js
+++ app.js
@@ -3,6 +3,27 @@
 // Execute this code only AFTER the document is ready
 // Hint: use jQuery's `$(document).ready`
 
+  function Game() {
+    //Create a new instance of player 1
+    //this.player1 = ...
+
+    //Do the same for a player 2
+    //this.player2 = ...
+    
+    //Create the board
+    //this.board = ...
+  }
+
+  // Remember: prototypes are shared functions between all game instances
+  Game.prototype.nextPlayer = function() {
+    //Switch players
+  };
+
+  // `Game.prototype.init` kicks off a new game with a board and two players
+  Game.prototype.init = function() {
+    //
+  };
+
   // A starter Player constructor.
   function Player(team) {
     //Is the player X or O?
@@ -11,32 +32,12 @@
 
   // A starter Board constructor.
   function Board() {
-    //The board needs 2 players!
-
-    //Create a new instance of player 1 and store it in the board
-    //this.player1 = ...
-
-    //Do the same for a player 2
-    //this.player2 = ...
-
-    //Tracks the cells of the board
+    //Tracks the cells of the board instance
     //this.$cells = ...
 
-    //Store any other properties that board may have below
-    //this includes a reset option, currentPlayer, counter, etc
-  };
-
-  // Remember: prototypes are shared functions between all board instances
-  Board.prototype.nextPlayer = function() {
-    //Switch players
-  };
-
-  // `Board.prototype.init` initializes our event listeners
-  Board.prototype.init = function() {
-    //Initialize the event listeners
+    //Store any other properties that board may have below, such as a reset option
   };
 
   // Start the game!
-  var board = new Board();
-  board.init();
-});
+  var game = new Game();
+  game.init();
